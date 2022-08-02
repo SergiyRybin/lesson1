5f6f687b6c6c3453d654c2739425933a528a3eca
SergiyRybin <serhiobax@gmail.com>
2022-08-02 11:12:07 +0300

    upd

---
+++ contacts.js
@@ -0,0 +1,38 @@
+const fs = require("fs/promises");
+const path = require("path");
+const contactsPath = path.join(__dirname, "db/contacts.json");
+
+// // TODO: задокументировать каждую функцию
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  const products = JSON.parse(data);
+  return products;
+}
+
+async function getContactById(contactId) {
+  const allContacts = await listContacts();
+  const result = await allContacts.find(
+    (e) => e.id.toString() === contactId.toString()
+  );
+  if (!result) return null;
+  return result;
+}
+
+async function removeContact(contactId) {
+  const allContacts = await listContacts();
+  const delContacts = allContacts.filter(
+    (e) => e.id.toString() !== contactId.toString()
+  );
+  await fs.writeFile(contactsPath, JSON.stringify(delContacts))
+  return delContacts;
+}
+
+async function addContact(name, email, phone) {
+  const allContacts = await listContacts();
+  const newContact = { name, email, phone, id: 123 };
+  allContacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(allContacts));
+  return newContact;
+}
+
+module.exports = { listContacts, getContactById, addContact, removeContact };
---
+++ index.js
@@ -0,0 +1,56 @@
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+} = require("./contacts");
+
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+const argv = program.opts();
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const products = await listContacts();
+      console.log(products);
+      break;
+    case "get":
+      const productById = await getContactById(id);
+      console.log(productById);
+      if (!productById) {
+        throw new Error(`Product with id=${id} not found `);
+      }
+      break;
+    case "add":
+      const addProducts = await addContact(name, email, phone);
+      console.log(addProducts);
+      break;
+    case "remove":
+      const contactId = await getContactById(id);
+      if (!contactId) {
+        throw new Error(`Product with id=${id} not found `);
+      } else {
+        const removeById = await removeContact(id);
+        console.log(removeById);
+      }
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+const name = "Mary";
+const email = "facke";
+const phone = "(987) 654321";
+
+invokeAction(argv);
